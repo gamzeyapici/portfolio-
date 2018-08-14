d69d99a7caf091bba10135c39a53ec547f9a7878
worlfee <worlfee@gmail.com>
2018-08-15 00:55:06 +0300

    page of template

--- app/components/navbar/navbar.component.js
+++ app/components/navbar/navbar.component.js
@@ -9,4 +9,12 @@
             templateUrl: 'app/components/navbar/navbar.template.html',
             controller: ['$scope', NavbarController]
         });
+
+        // function openNav() {
+        //     document.getElementById("myNav").style.height = "100%";
+        // }
+        
+        // function closeNav() {
+        //     document.getElementById("myNav").style.height = "0%";
+        // }
 })();
