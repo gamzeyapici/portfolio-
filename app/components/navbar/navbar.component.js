d66ea3d0070a84d10ca928d8692f8976f646420a
gamze <worlfee@gmail.com>
2018-08-15 16:38:27 +0300

    template

--- app/components/navbar/navbar.component.js
+++ app/components/navbar/navbar.component.js
@@ -17,4 +17,14 @@
         // function closeNav() {
         //     document.getElementById("myNav").style.height = "0%";
         // }
+
+
+
+        function openNav() {
+            document.getElementById("myNav").style.height = "100%";
+        }
+        
+        function closeNav() {
+            document.getElementById("myNav").style.height = "0%";
+        }
 })();
